001823f42a28217a3cf52514f54a0e9e0967ba0e
Makenna Schulz <mmschulz30@gmail.com>
2018-07-17 09:58:29 -0600

    integrated font awesome and built a custom icon component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course__title">Problem Solving</label>
-
+                { Icon('fas fa-check', 'library-course__icon')}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam hendrerit nulla ac tincidunt ultrices. Nulla placerat neque est, nec dapibus augue mollis porta. Nunc sollicitudin accumsan laoreet. Vivamus posuere eleifend dui eu facilisis.</p>
